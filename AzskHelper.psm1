5945d06e99abf8ede301ad8a2feab594584234e9
Martin Palmer <martin.palmer@almmechanics.com>
2019-03-19 07:36:30 +0000

    initial module shape

---
+++ AzskHelper.psm1
@@ -0,0 +1,6 @@
+Set-StrictMode -Version Latest
+$cmdletsPath = Join-Path $PSScriptRoot 'cmdlets'
+$interfacePath = Join-Path $cmdletsPath 'interface'
+# . (Join-Path $interfacePath '<interface_sample>.ps1')
+$internalPath = Join-Path $cmdletsPath 'internal'
+# . (Join-Path $internalPath '<internal_sample>.ps1')
